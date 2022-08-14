cdb3b0e071ad3da2f948996cc95aa419a2900dba
jaeyson <hello@jaeyson.dev>
2022-08-14 15:15:33 +0800

    add deps

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Algox.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
       deps: deps()
     ]
   end
@@ -21,6 +22,11 @@ defmodule Algox.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.11", only: :dev},
+      {:stream_data, "~> 0.5", only: :test},
+      {:excoveralls, "~> 0.14", only: :test},
+      {:mix_audit, "~> 2.0", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
